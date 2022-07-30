9f2d16f17575e4bd12fbe405f28ea2d99a0e351c
Maksim Kastsiuchenka <maksim.kastsiuchenka@leverx.com>
2022-07-30 20:03:38 +0400

    Initial project setup

---
+++ services/backend/service/api/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+
+from apps.admin.admins import admin_site
+
+from .v1.urls import urlpatterns as v1_urls
+from .health_check import urlpatterns as health_check_urls
+from .docs import urlpatterns as docs_urls
+
+
+urlpatterns = [
+    path('admin/', admin_site.urls),
+    path('zZj55YsP8vVXdsmK/', include(docs_urls)),
+    path('api/', include([
+        path('v1/', include(v1_urls)),
+        path('health_check/', include(health_check_urls)),
+    ]))
+] + (
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
