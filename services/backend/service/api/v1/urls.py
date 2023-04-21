65ed7bbd69837118fc7af8b67102606ef3d9821a
mox8 <makxim.spain@icloud.com>
2023-04-21 16:44:43 +0200

    Docs moved under the api version

--- services/backend/service/api/v1/urls.py
+++ services/backend/service/api/v1/urls.py
@@ -1,3 +1,25 @@
 from django.urls import path
+from django.conf import settings
+from rest_framework import permissions
 
-urlpatterns = []
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+_urlpatterns = []
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title=settings.PROJECT_NAME,
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    patterns=_urlpatterns,
+)
+
+urlpatterns = _urlpatterns + [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+]
